e307caa0d2deeb54585b75adcca46e7f2a15e2a2
Keith Fletcher <keith@fletcher.org>
2016-03-10 23:00:24 +1300

    Tidying up CircularBuffer. Add snippet file

--- C#/CircularBuffer.cs
+++ C#/CircularBuffer.cs
@@ -1,25 +1,79 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gallagher.Utilities
+/*
+    A circular buffer implementation for C#
+    https://en.wikipedia.org/wiki/Circular_buffer
+    
+        The buffer can be initialised with OverWriting either enabled or
+        disabled (disabled by default). If it is enabled, an exception 
+        will be thrown if the buffer capacity is exceeded, otherwise it 
+        will simply drop as many elements off the back as needed in order 
+        to stay within the capacity limit.
+
+        Data is added singularly with Add() and removed singulary with 
+        Remove(). The usual ICollection methods all work as expected.
+
+        Read() and Write() have been provided to extract or add blocks
+        of data respectively.
+
+    Keith Fletcher
+    Mar 2016
+    
+    This file is Unlicensed. 
+    See the foot of the file, or refer to <http://unlicense.org>
+*/
+
+namespace fletcher.org
 {
     public class CircularBuffer<T> : ICollection<T>
     {
         #region Constructors
+        /// <summary>
+        /// Create a new instance of <see cref="CircularBuffer{T}"/>,
+        /// with a default <see cref="Capacity"/> of 1024. 
+        /// <see cref="CanOverwrite"/> defaults to false.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="capacity"/> is not larger than 0.</exception>
         public CircularBuffer()
-            : this(DEFAULT_CAPACITY)
+            : this(DEFAULT_CAPACITY, false)
         { }
 
+        /// <summary>
+        /// Create a new instance of <see cref="CircularBuffer{T}"/>,
+        /// with the specified <paramref name="capacity"/>. 
+        /// <see cref="CanOverwrite"/> defaults to false.
+        /// </summary>
+        /// <param name="capacity">The maximum number of elements that the 
+        /// <see cref="CircularBuffer{T}"/> can hold</param>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="capacity"/> is not larger than 0.</exception>
+        public CircularBuffer(int capacity)
+            : this(capacity, false)
+        { }
+
+        /// <summary>
+        /// Create a new instance of <see cref="CircularBuffer{T}"/>,
+        /// with the specified <paramref name="capacity"/>. 
+        /// </summary>
+        /// <param name="capacity">The maximum number of elements that the 
+        /// <see cref="CircularBuffer{T}"/> can hold</param>
+        /// <param name="overwrite">If <paramref name="overwrite"/> is true, an <see cref="ArgumentException"/>
+        /// is thrown if you exceed the buffer size. When <paramref name="overwrite"/>
+        /// is false, newer data overwrites the older elements in the buffer.</param>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="capacity"/> is not larger than 0.</exception>
         public CircularBuffer(int capacity, bool overwrite = false)
         {
+            Contract.Requires<ArgumentOutOfRangeException>(capacity >= 0, $"{nameof(capacity)} must be larger than zero.");
             if (capacity < 0)
                 throw new ArgumentOutOfRangeException(nameof(capacity), $"{nameof(capacity)} must be larger than zero.");
+
             _overwrite = overwrite;
             _capacity = capacity;
             _buffer = new T[_capacity];
@@ -27,13 +81,28 @@ namespace Gallagher.Utilities
         #endregion Constructors
 
         #region ICollection<T> implementation
+        /// <summary>
+        /// Returns the actual number of elements in the <see cref="CircularBuffer{T}"/>
+        /// </summary>
+        /// <returns>The actual number of elements in the <see cref="CircularBuffer{T}"/></returns>
         public int Count =>
             _readIndex == _writeIndex
                 ? (_isFull ? _capacity : 0)
                 : (_readIndex > _writeIndex ? _capacity : 0) + _writeIndex - _readIndex;
 
+        /// <summary>
+        /// Always returns false. The <see cref="CircularBuffer{T}"/>
+        /// cannot be made read-only.
+        /// </summary>
+        /// <returns>False</returns>
         public bool IsReadOnly => false;
 
+        /// <summary>
+        /// Add <paramref name="item"/> to the <see cref="CircularBuffer{T}"/>.
+        /// </summary>
+        /// <param name="item">The item to be added.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="item"/> is null.</exception>
+        /// <exception cref="InvalidOperationException">The buffer is already full and <see cref="CanOverwrite"/> is false.</exception>
         public void Add(T item)
         {
             if (item == null)
@@ -48,17 +117,25 @@ namespace Gallagher.Utilities
                 _isFull = (_writeIndex == _readIndex);
         }
 
+        /// <summary>
+        /// Removes all elements from teh buffer.
+        /// </summary>
         public void Clear() 
         {
             _readIndex = 0;
             _writeIndex = 0;
         }
 
-        /// <summary>
-        /// Not implemented
-        /// </summary>
         public bool Contains(T item)
-        { throw new NotImplementedException(); }
+        {
+            var index = _readIndex;
+            for (var i = 0; i < Count; ++i)
+                if (_buffer[IndexInc(ref index)] == item)
+                    return true;
+
+                //return ;
+
+        }
 
         /// <summary>
         /// Not implemented
@@ -114,6 +191,8 @@ namespace Gallagher.Utilities
                 yield return Remove();
         }
         #endregion ICollection<T> implementation
+
+        public bool CanOverwrite => _overwrite;
 
         /// <summary>
         /// Remove a single <typeparamref name="T"/> instance from the buffer and return it.
@@ -228,51 +307,32 @@ namespace Gallagher.Utilities
         int _readIndex = 0;
         int _writeIndex = 0;
     }
-
-    public class CircularByteBuffer : CircularBuffer<byte>
-    {
-        public CircularByteBuffer()
-            : base()
-        { }
-
-        public CircularByteBuffer(int capacity, bool overwrite = false)
-            : base(capacity, overwrite)
-        { }
-
-        // serial.Read(buffer, writeOffset, bytesToRead);
-
-        //public static implicit operator byte[] (CircularByteBuffer buffer)
-        //{
-        //    return new byte[0];
-        //}
-
-        //public static implicit operator CircularByteBuffer(byte[] buffer)
-        //{
-        //    return new CircularByteBuffer();
-        //}
-    }
-
-    public static class CircularBufferExtensions
-    {
-        public static int Read(this SerialPort serial, CircularByteBuffer buffer, int offset, int count)
-        {
-            var data = new byte[count];
-            var bytesRead = serial.Read(data, 0, data.Length);
-            if (bytesRead > 0)
-                buffer.Write(data, 0, bytesRead);
-            return bytesRead;
-        }
-
-        public static int Read(this SerialPort serial, CircularByteBuffer buffer, int offset, int count, Stream stream)
-        {
-            var data = new byte[count];
-            var bytesRead = serial.Read(data, 0, data.Length);
-            if (bytesRead > 0)
-            {
-                buffer.Write(data, 0, bytesRead);
-                stream.Write(data, 0, bytesRead);
-            }
-            return bytesRead;
-        }
-    }
 }
+
+
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+*/
