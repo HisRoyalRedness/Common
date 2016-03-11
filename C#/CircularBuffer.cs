53ac6eaa59272c75d6121bcd89e9a4a6d086444c
Keith Fletcher (Work) <keith@fletcher.org>
2016-03-11 19:42:02 +1300

    Add XML docs, and start of unit tests

--- C#/CircularBuffer.cs
+++ C#/CircularBuffer.cs
@@ -1,24 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.IO;
-using System.IO.Ports;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
     A circular buffer implementation for C#
     https://en.wikipedia.org/wiki/Circular_buffer
-    
+
         The buffer can be initialised with OverWriting either enabled or
-        disabled (disabled by default). If it is enabled, an exception 
-        will be thrown if the buffer capacity is exceeded, otherwise it 
-        will simply drop as many elements off the back as needed in order 
+        disabled (disabled by default). If it is enabled, an exception
+        will be thrown if the buffer capacity is exceeded, otherwise it
+        will simply drop as many elements off the back as needed in order
         to stay within the capacity limit.
 
-        Data is added singularly with Add() and removed singulary with 
+        Data is added singularly with Add() and removed singulary with
         Remove(). The usual ICollection methods all work as expected.
 
         Read() and Write() have been provided to extract or add blocks
@@ -26,20 +21,20 @@ using System.Threading.Tasks;
 
     Keith Fletcher
     Mar 2016
-    
-    This file is Unlicensed. 
+
+    This file is Unlicensed.
     See the foot of the file, or refer to <http://unlicense.org>
 */
 
 namespace fletcher.org
 {
-    public class CircularBuffer<T> : ICollection<T>
+    public class CircularBuffer<T> : ICollection<T>, IEnumerable<T>
     {
         #region Constructors
         /// <summary>
         /// Create a new instance of <see cref="CircularBuffer{T}"/>,
-        /// with a default <see cref="Capacity"/> of 1024. 
-        /// <see cref="CanOverwrite"/> defaults to false.
+        /// with a default <see cref="Capacity"/> of 1024.
+        /// <see cref="Overwrite"/> defaults to false.
         /// </summary>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="capacity"/> is not larger than 0.</exception>
         public CircularBuffer()
@@ -48,10 +43,10 @@ namespace fletcher.org
 
         /// <summary>
         /// Create a new instance of <see cref="CircularBuffer{T}"/>,
-        /// with the specified <paramref name="capacity"/>. 
-        /// <see cref="CanOverwrite"/> defaults to false.
+        /// with the specified <paramref name="capacity"/>.
+        /// <see cref="Overwrite"/> defaults to false.
         /// </summary>
-        /// <param name="capacity">The maximum number of elements that the 
+        /// <param name="capacity">The maximum number of elements that the
         /// <see cref="CircularBuffer{T}"/> can hold</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="capacity"/> is not larger than 0.</exception>
         public CircularBuffer(int capacity)
@@ -60,9 +55,9 @@ namespace fletcher.org
 
         /// <summary>
         /// Create a new instance of <see cref="CircularBuffer{T}"/>,
-        /// with the specified <paramref name="capacity"/>. 
+        /// with the specified <paramref name="capacity"/>.
         /// </summary>
-        /// <param name="capacity">The maximum number of elements that the 
+        /// <param name="capacity">The maximum number of elements that the
         /// <see cref="CircularBuffer{T}"/> can hold</param>
         /// <param name="overwrite">If <paramref name="overwrite"/> is true, an <see cref="ArgumentException"/>
         /// is thrown if you exceed the buffer size. When <paramref name="overwrite"/>
@@ -70,11 +65,10 @@ namespace fletcher.org
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="capacity"/> is not larger than 0.</exception>
         public CircularBuffer(int capacity, bool overwrite = false)
         {
-            Contract.Requires<ArgumentOutOfRangeException>(capacity >= 0, $"{nameof(capacity)} must be larger than zero.");
-            if (capacity < 0)
-                throw new ArgumentOutOfRangeException(nameof(capacity), $"{nameof(capacity)} must be larger than zero.");
+            if (capacity <= 0)
+                throw new ArgumentOutOfRangeException($"{nameof(capacity)} must be larger than 0.");
 
-            _overwrite = overwrite;
+            Overwrite = overwrite;
             _capacity = capacity;
             _buffer = new T[_capacity];
         }
@@ -101,50 +95,68 @@ namespace fletcher.org
         /// Add <paramref name="item"/> to the <see cref="CircularBuffer{T}"/>.
         /// </summary>
         /// <param name="item">The item to be added.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="item"/> is null.</exception>
-        /// <exception cref="InvalidOperationException">The buffer is already full and <see cref="CanOverwrite"/> is false.</exception>
-        public void Add(T item)
-        {
-            if (item == null)
-                throw new ArgumentNullException(nameof(item));
-            if (_isFull && !_overwrite)
-                throw new InvalidOperationException($"Cannot {nameof(Add)} another {nameof(item)}. The {nameof(CircularBuffer<T>)} is full.");
-
-            _buffer[IndexInc(ref _writeIndex)] = item;
-            if (_isFull)
-                _readIndex = _writeIndex;
-            else
-                _isFull = (_writeIndex == _readIndex);
-        }
+        /// <exception cref="InvalidOperationException">The buffer is already full
+        /// and <see cref="Overwrite"/> is false.</exception>
+        public void Add(T item) => Add(item, Overwrite);
 
         /// <summary>
-        /// Removes all elements from teh buffer.
+        /// Removes all elements from the buffer.
         /// </summary>
-        public void Clear() 
+        public void Clear()
         {
             _readIndex = 0;
             _writeIndex = 0;
         }
 
+        /// <summary>
+        /// Looks for an item in the <see cref="CircularBuffer{T}"/>
+        /// using item.Equals().
+        /// <param name="item">The item to search for.</param>
+        /// <returns>Returns true if an item was found that equals <paramref name="item"/>.
+        /// Returns false otherwise, or if <paramref name="item"/> is null.</returns>
         public bool Contains(T item)
         {
-            var index = _readIndex;
+            if (item == null)
+                return false;
             for (var i = 0; i < Count; ++i)
-                if (_buffer[IndexInc(ref index)] == item)
+                if (item.Equals(this[i]))
                     return true;
-
-                //return ;
-
+            return false;
         }
 
         /// <summary>
-        /// Not implemented
+        /// Not supported
         /// </summary>
-        public bool Remove(T item)
-        { throw new NotImplementedException(); }
+        bool ICollection<T>.Remove(T item)
+        { throw new NotSupportedException(); }
 
-        public void CopyTo(T[] data, int offset) => CopyTo(data, offset, Count);        
+        /// <summary>
+        /// Copy data from the <see cref="CircularBuffer{T}"/> to an array.
+        /// The items are not removed from the <see cref="CircularBuffer{T}"/>.
+        /// Use <see cref="Read(T[], int, int)"/> if you want to copy items
+        /// and remove them from the <see cref="CircularBuffer{T}"/>.
+        /// </summary>
+        /// <param name="data">The array to copy into.</param>
+        /// <param name="offset">The offset in <paramref name="data"/> at which to begin copying.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="data"/> is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is negative.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">There is not enough space in <paramref name="data"/> 
+        /// (starting from <paramref name="offset"/>) to hold all the data.</exception>
+        public void CopyTo(T[] data, int offset) => CopyTo(data, offset, Count);
 
+        /// <summary>
+        /// Copy data from the <see cref="CircularBuffer{T}"/> to an array.
+        /// The items are not removed from the <see cref="CircularBuffer{T}"/>.
+        /// Use <see cref="Read(T[], int, int)"/> if you want to copy items
+        /// and remove them from the <see cref="CircularBuffer{T}"/>.
+        /// </summary>
+        /// <param name="data">The array to copy into.</param>
+        /// <param name="offset">The offset in <paramref name="data"/> at which to begin copying.</param>
+        /// <param name="length">The number of elements to copy.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="data"/> is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is negative.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">There is not enough space in <paramref name="data"/> 
+        /// (starting from <paramref name="offset"/>) to hold all the data.</exception>
         public void CopyTo(T[] data, int offset, int length)
         {
             if (length == 0)
@@ -172,27 +184,59 @@ namespace fletcher.org
         }
 
         /// <summary>
-        /// Each iteration will read from the current read index,
-        /// while Count is greater than zero.
+        /// Iterate over the elements in the <see cref="CircularBuffer{T}"/>.
+        /// The elements are not removed. Altering the buffer (e.g. Add, Remove
+        /// or Clear) will invalidate the enumerator.
         /// </summary>
+        /// <returns>An <see cref="IEnumerator{T}"/> for the <see cref="CircularBuffer{T}"/>.</returns>
         public IEnumerator<T> GetEnumerator()
         {
-            while (Count > 0)
-                yield return Remove();
+            for (var i = 0; i < Count; ++i)
+                yield return this[i];
         }
 
         /// <summary>
-        /// Each iteration will read from the current read index,
-        /// while Count is greater than zero.
+        /// Iterate over the elements in the <see cref="CircularBuffer{T}"/>.
+        /// The elements are not removed. Altering the buffer (e.g. Add, Remove
+        /// or Clear) will invalidate the enumerator.
         /// </summary>
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            while (Count > 0)
-                yield return Remove();
-        }
+        /// <returns>An <see cref="IEnumerator"/> for the <see cref="CircularBuffer{T}"/>.</returns>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         #endregion ICollection<T> implementation
 
-        public bool CanOverwrite => _overwrite;
+        /// <summary>
+        /// If true, an item added to a full buffer will cause
+        /// an item to be dropped from the end of the <see cref="CircularBuffer{T}"/>.
+        /// If false, and item added to a full <see cref="CircularBuffer{T}"/>
+        /// will result in an <see cref="InvalidOperationException"/>.
+        /// </summary>
+        public bool Overwrite { get; set; }
+
+        /// <summary>
+        /// The maximum number of items the <see cref="CircularBuffer{T}"/> can hold.
+        /// </summary>
+        public int Capacity => _capacity;
+
+        /// <summary>
+        /// Add <paramref name="item"/> to the <see cref="CircularBuffer{T}"/>.
+        /// </summary>
+        /// <param name="item">The item to be added.</param>
+        /// <param name="overwrite">If true, an Add to a full <see cref="CircularBuffer{T}"/> will drop
+        /// an element from the end. If false, an Add to a full <see cref="CircularBuffer{T}"/> will
+        /// result in an <see cref="InvalidOperationException"/>.</param>
+        /// <exception cref="InvalidOperationException">The buffer is already full and <paramref name="overwrite"/> 
+        /// is false.</exception>
+        public void Add(T item, bool overwrite)
+        {
+            if (_isFull && !overwrite)
+                throw new InvalidOperationException($"Cannot {nameof(Add)} another {nameof(item)}. The {nameof(CircularBuffer<T>)} is full.");
+
+            _buffer[IndexInc(ref _writeIndex)] = item;
+            if (_isFull)
+                _readIndex = _writeIndex;
+            else
+                _isFull = (_writeIndex == _readIndex);
+        }
 
         /// <summary>
         /// Remove a single <typeparamref name="T"/> instance from the buffer and return it.
@@ -207,6 +251,46 @@ namespace fletcher.org
             return _buffer[IndexInc(ref _readIndex)];
         }
 
+        /// <summary>
+        /// Return a single item from the <see cref="CircularBuffer{T}"/>
+        /// without removing it.
+        /// </summary>
+        /// <param name="index">The zero-based index of the item.</param>
+        /// <returns>The item at the zero-based index in the <see cref="CircularBuffer{T}"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/> is negative.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/> exceeds <see cref="Count"/>.</exception>
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0)
+                    throw new ArgumentOutOfRangeException(nameof(index), $"{nameof(index)} must not be negative.");
+                if (index > Count)
+                    throw new ArgumentOutOfRangeException(nameof(index), $"{nameof(index)} cannot exceed {nameof(Count)}.");
+
+                var newIndex = _readIndex + index;
+                return newIndex >= _capacity
+                    ? _buffer[newIndex - _capacity]
+                    : _buffer[newIndex];
+            }
+        }
+
+        /// <summary>
+        /// Remove items from the <see cref="CircularBuffer{T}"/>, and copies
+        /// them into <paramref name="data"/>.
+        /// Use <see cref="CopyTo(T[], int, int)"/> is you want to copy items
+        /// without removing them from the <see cref="CircularBuffer{T}"/>.
+        /// </summary>
+        /// <param name="data">The array to copy the items into.</param>
+        /// <param name="offset">The offset in <paramref name="data"/> at which to begin copying.</param>
+        /// <param name="length">The number of items to copy from the <see cref="CircularBuffer{T}"/>.
+        /// Fewer then <paramref name="length"/> items may be copied if there aren't enough items in the
+        /// <see cref="CircularBuffer{T}"/>.
+        /// <returns>The number of items copied to <paramref name="data"/> and removed from <see cref="CircularBuffer{T}"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="data"/> is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is negative.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">There is not enough space in <paramref name="data"/> 
+        /// (starting from <paramref name="offset"/>) to hold all the data.</exception>
         public int Read(T[] data, int offset, int length)
         {
             var len = length > Count ? Count : length;
@@ -222,7 +306,7 @@ namespace fletcher.org
 
         /// <summary>
         /// Extract <paramref name="length"/> items from <paramref name="data"/>,
-        /// starting at <paramref name="offset"/>, then add them to the 
+        /// starting at <paramref name="offset"/>, then add them to the
         /// <see cref="CircularBuffer{T}"/> instance.
         /// </summary>
         /// <exception cref="System.ArgumentNullException"><paramref name="data"/> is null.</exception>
@@ -241,7 +325,7 @@ namespace fletcher.org
                 throw new ArgumentException($"{nameof(data)} is smaller than {nameof(length)}.");
             if (data.Length - offset < length)
                 throw new ArgumentException($"{nameof(data)} is smaller than {nameof(length)} with the provided {nameof(offset)}.");
-            if (length + Count > _capacity && !_overwrite)
+            if (length + Count > _capacity && !Overwrite)
                 throw new ArgumentException($"Not enough space in the {nameof(CircularBuffer<T>)} for the {nameof(length)} given.");
 
             _isFull = length + Count >= _capacity;
@@ -273,9 +357,7 @@ namespace fletcher.org
                 _readIndex = _writeIndex;
         }
 
-        public int Capacity => _capacity;
-
-        #region Index manipulation
+        #region Internal index manipulation
         /// <summary>
         /// ++index
         /// </summary>
@@ -296,11 +378,10 @@ namespace fletcher.org
                 index = 0;
             return ind;
         }
-        #endregion
+        #endregion Internal index manipulation
 
         const int DEFAULT_CAPACITY = 1024;
 
-        bool _overwrite = false;
         bool _isFull = false;
         int _capacity;
         T[] _buffer;
---
+++ Test/Common_Test/CircularBuffer_Test.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using fletcher.org;
+
+namespace fletcher.org.Tests
+{
+    [TestClass]
+    public class CircularBuffer_Test
+    {
+        [TestMethod]
+        public void TestConstruction()
+        {
+            var capacity = 1024;
+            var overwrite = false;
+            var cb = new CircularBuffer<int>();
+            Assert.AreEqual(capacity, cb.Capacity, $"Default Capacity should be {capacity}.");
+            Assert.AreEqual(overwrite, cb.Overwrite, $"Default Overwrite should be {overwrite}.");
+
+            capacity = 100;
+            cb = new CircularBuffer<int>(capacity);
+            Assert.AreEqual(capacity, cb.Capacity, "Specified Capacity should be {capacity}.");
+            Assert.AreEqual(overwrite, cb.Overwrite, $"Default Overwrite should be {overwrite}.");
+
+            capacity = 200;
+            overwrite = true;
+            cb = new CircularBuffer<int>(capacity, overwrite);
+            Assert.AreEqual(capacity, cb.Capacity, "Specified Capacity should be {capacity}.");
+            Assert.AreEqual(overwrite, cb.Overwrite, $"Specified Overwrite should be {overwrite}.");
+        }
+
+        [TestMethod]
+        public void Test_Add()
+        {
+            // First try overwrite defaulted to true
+            var capacity = 5;
+            var overwrite = true;
+            var expectedCount = 0;
+            var cb = new CircularBuffer<int>(capacity, overwrite);
+            Assert.AreEqual(0, cb.Count, $"Count should start off at 0.");
+
+            cb.Add(1);
+            expectedCount = 1;
+            Assert.AreEqual(expectedCount, cb.Count, $"Count should be {expectedCount}.");
+
+            cb.Add(2);
+            cb.Add(3);
+            cb.Add(4);
+            cb.Add(5);
+            expectedCount = 5;
+            Assert.AreEqual(expectedCount, cb.Count, $"Count should be {expectedCount}.");
+            var items = string.Join(".", cb.Select(i => i.ToString()));
+            Assert.AreEqual("1.2.3.4.5", items, "Checking the elements in the buffer");
+
+            // Overwrite should be allowed
+            cb.Add(6);
+            Assert.AreEqual(expectedCount, cb.Count, $"Count should not have increased from {expectedCount}.");
+            items = string.Join(".", cb.Select(i => i.ToString()));
+            Assert.AreEqual("2.3.4.5.6", items, "Checking the elements in the buffer");
+
+            // Overwrite should not be allowed
+            AssertExt.MustThrow<InvalidOperationException>(() => cb.Add(7, false));
+            Assert.AreEqual(expectedCount, cb.Count, $"Count should not have increased from {expectedCount}.");
+            items = string.Join(".", cb.Select(i => i.ToString()));
+            Assert.AreEqual("2.3.4.5.6", items, "Checking the elements in the buffer");
+
+            // Now try with overwrite defaulted to false
+            overwrite = false;
+            cb = new CircularBuffer<int>(capacity, overwrite);
+
+            cb.Add(1);
+            cb.Add(2);
+            cb.Add(3);
+            cb.Add(4);
+            cb.Add(5);
+            expectedCount = 5;
+            Assert.AreEqual(expectedCount, cb.Count, $"Count should be {expectedCount}.");
+            items = string.Join(".", cb.Select(i => i.ToString()));
+            Assert.AreEqual("1.2.3.4.5", items, "Checking the elements in the buffer");
+
+            // Overwrite should not be allowed
+            AssertExt.MustThrow<InvalidOperationException>(() => cb.Add(6));
+            Assert.AreEqual(expectedCount, cb.Count, $"Count should not have increased from {expectedCount}.");
+            items = string.Join(".", cb.Select(i => i.ToString()));
+            Assert.AreEqual("1.2.3.4.5", items, "Checking the elements in the buffer");
+
+            // Overwrite should be allowed
+            cb.Add(7, true);
+            Assert.AreEqual(expectedCount, cb.Count, $"Count should not have increased from {expectedCount}.");
+            items = string.Join(".", cb.Select(i => i.ToString()));
+            Assert.AreEqual("2.3.4.5.7", items, "Checking the elements in the buffer");
+
+            cb.Overwrite = true;
+            cb.Add(8);
+            Assert.AreEqual(expectedCount, cb.Count, $"Count should not have increased from {expectedCount}.");
+            items = string.Join(".", cb.Select(i => i.ToString()));
+            Assert.AreEqual("3.4.5.7.8", items, "Checking the elements in the buffer");
+        }
+
+        [TestMethod]
+        public void Test_Misc()
+        {
+            var cb = new CircularBuffer<int>();
+            Assert.AreEqual(false, cb.IsReadOnly, $"ReadOnly should always be false.");
+        }
+    }
+
+    #region Test Extensions
+    public static class AssertExt
+    {
+        public static void MustThrow<TException>(Action action, string message = "")
+            where TException : Exception
+        {
+            try
+            {
+                action();
+                Assert.Fail($"Expected a {typeof(TException).Name} to be thrown.");
+            }
+            catch(TException)
+            {
+                // All good!
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail($"Caught an unexpected {ex.GetType().Name}. {ex}");
+            }
+        }
+    }
+    #endregion Test Extensions
+}
