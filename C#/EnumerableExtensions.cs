0c1cdf9066ec1c87001156ef15801e31ba69aa59
HisRoyalRedness <keith@fletcher.org>
2017-11-17 19:05:23 +1300

    Updates:

---
+++ C#/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HisRoyalRedness.com
+{
+    internal static class EnumerableExtensions
+    {
+        internal static T Tap<T>(this T item, Action<T> action)
+        {
+            action(item);
+            return item;
+        }
+    }
+}
